4b71ec71c23e49c0f67f6151b6ec38455e50ca19
Pedro David Rocha Saldanha <87939906+PedroDavid2001@users.noreply.github.com>
2024-02-05 10:56:39 -0300

    Update sight.py

--- sight.py
+++ sight.py
@@ -4,32 +4,53 @@ class Sight(pygame.sprite.Sprite):
     def __init__(self, janela):
         pygame.sprite.Sprite.__init__(self) 
         self.sprites = []
-        self.curr = 0
+        self.effects = []
+        self.rects = []
+        self.fx_rects = []
+        self.curr = 2
         
-        self.sprites.append( pygame.image.load('assets/eye.png') )
-        self.sprites.append( pygame.image.load('assets/sight.png') )
+        # Imagens do cursor
         self.sprites.append( pygame.image.load('assets/punch.png') )
+        self.sprites.append( pygame.image.load('assets/sight.png') )
+        self.sprites.append( pygame.image.load('assets/eye.png') )
         self.sprites.append( pygame.image.load('assets/take.png') )
         
-        self.image = self.sprites[0]
+        for i, img in enumerate(self.sprites):
+            self.rects.append(img.get_rect())
+            self.rects[i].topleft = 0, 0
+
+        # Imagens dos efeitos de interações do cursor
+        self.effects.append( pygame.image.load('assets/punch_fx.png') )
+        self.effects.append( pygame.image.load('assets/sight_fx.png') )
+        
+        for i, img in enumerate(self.effects):
+            self.fx_rects.append(img.get_rect())
+            self.fx_rects[i].topleft = 0, 0
+
+        #Atributos adicionais
         self.screen = janela
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 0, 0
         self.clicking = False
    
     def update(self, event):
    
+        self.clicking = False
+
         if( event.type == pygame.MOUSEWHEEL ):
             if event.y < 0:
                 if self.curr > 0:
                     self.curr -= 1
-            else:
-                if self.curr < (len( self.sprites ) - 1):
+            elif self.curr < (len( self.sprites ) - 1):
                     self.curr += 1
+        elif( event.type == pygame.MOUSEBUTTONDOWN ):
+            self.clicking = True
 
-        self.image = self.sprites[ self.curr ]
         mouse_pos = pygame.mouse.get_pos()
-        self.rect.center = mouse_pos[0], mouse_pos[1]
+        self.rects[self.curr].center = mouse_pos[0], mouse_pos[1]
+        if(self.clicking is True and self.curr < len( self.effects )):
+            self.fx_rects[self.curr].center = mouse_pos[0], mouse_pos[1]
 
     def draw(self):
-        self.screen.blit(self.image, self.rect, None)
+        self.screen.blit(self.sprites[self.curr], self.rects[self.curr], None)
+        
+        if(self.clicking is True and self.curr < len( self.effects )):
+            self.screen.blit(self.effects[self.curr], self.fx_rects[self.curr], None)
