35f24935f97f76122bcb32bf58b41a35a38927a5
PedroDavid2001 <pedrodavirochasaldanha@gmail.com>
2024-02-16 13:44:15 -0300

    teste de colisao no npc

--- npc.py
+++ npc.py
@@ -12,8 +12,11 @@ class NPC(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         self.screen = janela
+        # Array de imagens do npc
         self.images = []
+        # Array de booleans que definem se a imagem neste indice deve ser exibida
         self.display_img = []
+        # Array com retangulos individuais para cada imagem
         self.rects = []
         
         # Configura imagens
@@ -24,14 +27,24 @@ class NPC(pygame.sprite.Sprite):
             self.rects[i].topleft = 100, 100
         
     def update(self, x, y, curr_imgs):
-        
-        #curr_imgs armazena as posicoes das imagens que devem ser exibidas
+        # curr_imgs armazena as posicoes das imagens que devem ser exibidas
         for value in curr_imgs:
             self.display_img[value] = True
             self.rects[value].topleft = x, y
+        self.collision()
+   
+    def collision(self):
+        # Verifica colisao com mouse
+        [x, y] = pygame.mouse.get_pos() # [0] = x, [1] = y
+        for i, display in enumerate(self.display_img):
+            if display is True:
+                if pygame.Rect.collidepoint(self.rects[i], pygame.mouse.get_pos()):
+                    self.screen.blit( self.images[2], self.rects[2], None)
         
     def draw(self):
+        # Percorre o array de booleans para encontrar quais estao settados para True
         for i, display in enumerate(self.display_img):
             if display is True:
                 self.screen.blit( self.images[i], self.rects[i], None)
-                self.display_img[i] = False     # Reseta o valor para garantir que nao repetira na proxima iteracao
+                # Reseta o valor para garantir que nao repetira no proximo update
+                self.display_img[i] = False     
